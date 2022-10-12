b937722a745aa043ab732e5617636f79052faaa0
DownGrowth <1209157041@qq.com>
2022-10-12 23:02:46 +0800

    4个页面重构

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,21 +1,20 @@
-import { defineComponent } from 'vue';
-import s from './First.module.scss';
+import s from './WelcomeLayout.module.scss';
 import pig from '../../assets/icons/pig.svg';
 import { RouterLink } from 'vue-router';
-export const First = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img class={s.icon} src={pig} />
-          <h2>会挣钱<br/>还要会省钱</h2>
-        </div>
-        <div class={s.actions}>
+import { WelcomeLayout } from './WelcomeLayout';
+export const First =()=> (
+    <WelcomeLayout>
+      {{
+        icon: () => <img src={pig} />,
+        title: () => <h2>会赚钱<br />更要会省钱</h2>,
+        buttons: () => <>
           <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
           <RouterLink to="/welcome/2" >下一页</RouterLink>
           <RouterLink to="/start" >跳过</RouterLink>
-        </div>
-      </div>
-    )
-  }
-})
+        </>
+      }}
+    </WelcomeLayout>
+)
+First.displayName='First'
+   
+
--- src/components/welcome/Second.tsx
+++ src/components/welcome/Second.tsx
@@ -1,21 +1,19 @@
 import { defineComponent } from 'vue';
-import s from './First.module.scss';
+import s from './WelcomeLayout.module.scss';
 import clock from '../../assets/icons/clock.svg';
 import { RouterLink } from 'vue-router';
-export const Second = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img class={s.icon} src={clock} />
-          <h2>每日提醒<br/>不会遗漏每一笔账单</h2>
-        </div>
-        <div class={s.actions}>
+import { WelcomeLayout } from './WelcomeLayout';
+export const Second = ()=>(
+      <WelcomeLayout>
+        {{
+          icon: () => <img src={clock}/>,
+          title: () => <h2>每日提醒<br />不会遗漏每一笔账单</h2>,
+          buttons: () => <>
           <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
           <RouterLink to="/welcome/3" >下一页</RouterLink>
           <RouterLink to="/start" >跳过</RouterLink>
-        </div>
-      </div>
-    )
-  }
-})
+          </>
+        }}
+     </WelcomeLayout>
+)
+Second.displayName='Second'
