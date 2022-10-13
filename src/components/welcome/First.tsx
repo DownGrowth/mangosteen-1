84781ae2f121ee27d936de959c0ee591b9bd4142
DownGrowth <1209157041@qq.com>
2022-10-13 22:25:03 +0800

    整体引入svg图片

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,9 +1,10 @@
 import s from './welcome.module.scss';
-import pig from '../../assets/icons/pig.svg';
 
 export const First = () => {
   return <div class={s.card}>
-    <img src={pig} />
+     <svg>
+        <use xlinkHref='#pig'></use>
+      </svg>
     <h2>会挣钱<br />还会省钱</h2>
   </div>
 }
