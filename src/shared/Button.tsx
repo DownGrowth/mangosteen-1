8cfdcd424b671effc95f01edd697e588b22b079e
DownGrowth <1209157041@qq.com>
2022-10-27 19:21:03 +0800

    修复Button默认type

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -10,6 +10,7 @@ export const Button = defineComponent({
     },
     type: {
       type: String as PropType<"submit" | "button">,
+      default: "button",
     },
   },
   setup: (props, context) => {
--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { defineComponent, PropType, reactive } from "vue";
 import { MainLayout } from "../layouts/MainLayout";
 import { Button } from "../shared/Button";
@@ -15,8 +16,11 @@ export const SignInPage = defineComponent({
       email: [],
       code: [],
     });
-    const onClickSendValidationCode = () => {
-      console.log("1111");
+    const onClickSendValidationCode = async () => {
+      const response = await axios.post("/api/v1/validation_codes", {
+        email: formData.email,
+      });
+      console.log(response);
     };
     const onSubmit = (e: Event) => {
       e.preventDefault();
