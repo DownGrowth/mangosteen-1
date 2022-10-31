8231ad2b1a9723ab2b65a9389ac447ff168a53bc
DownGrowth <1209157041@qq.com>
2022-10-31 19:26:47 +0800

    跳过广告

--- src/components/welcome/Second.tsx
+++ src/components/welcome/Second.tsx
@@ -1,12 +1,18 @@
-import s from './welcome.module.scss';
-import { FunctionalComponent } from 'vue';
+import s from "./welcome.module.scss";
+import { FunctionalComponent } from "vue";
 
 export const Second: FunctionalComponent = () => {
-  return <div class={s.card}>
-    <svg>
-        <use xlinkHref='#clock'></use>
+  return (
+    <div class={s.card}>
+      <svg>
+        <use xlinkHref="#clock"></use>
       </svg>
-    <h2>每日提醒<br />不遗漏每一笔账单</h2>
-  </div>
-}
-Second.displayName='Second'
+      <h2>
+        每日提醒
+        <br />
+        不遗漏每一笔账单
+      </h2>
+    </div>
+  );
+};
+Second.displayName = "Second";
--- src/shared/SkipFeatures.tsx
+++ src/shared/SkipFeatures.tsx
@@ -1,13 +1,14 @@
 import { defineComponent, PropType } from "vue";
 import { RouteLocationRaw, Router, RouterLink } from "vue-router";
 export const SkipFeatures = defineComponent({
-  props: {
-    to: {
-      type: String as PropType<RouteLocationRaw>,
-      required: true,
-    },
-  },
   setup: (props, context) => {
-    return () => <RouterLink to={props.to}>跳过</RouterLink>;
+    const onClick = () => {
+      localStorage.setItem("skipFeatures", "yes");
+    };
+    return () => (
+      <span onClick={onClick}>
+        <RouterLink to="/start">跳过</RouterLink>
+      </span>
+    );
   },
 });
