f68b13c8ecfb362c082fa5aab33227b2a5102213
DownGrowth <1209157041@qq.com>
2022-11-06 20:43:31 +0800

    完成登录和退出登录

--- src/shared/me.tsx
+++ src/shared/me.tsx
@@ -1,16 +1,8 @@
 import { AxiosResponse } from "axios";
 import { http } from "./Http";
-export let mePromise:
-  | Promise<
-      AxiosResponse<{
-        resourse: {
-          id: number;
-        };
-      }>
-    >
-  | undefined;
+export let mePromise: Promise<AxiosResponse<Resource<User>>> | undefined;
 export const refreshMe = () => {
-  mePromise = http.get<{ resourse: { id: number } }>("/me");
+  mePromise = http.get<Resource<User>>("/me");
   return mePromise;
 };
 export const fetchMe = refreshMe;
