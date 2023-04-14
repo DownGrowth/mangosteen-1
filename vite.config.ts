1dc297e9fd37a3894b7253d72a55f2738a369a95
DownGrowth <1209157041@.com>
2023-04-14 15:10:57 +0800

    再次尝试接入后端https接口

--- vite.config.ts
+++ vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig(({ command }) => {
     server: {
       proxy: {
         "/api/v1": {
-          target: "https://mangosteen2.hunger-valley.com",
+          target: "http://121.196.236.94:3000/",
         },
       },
     },
